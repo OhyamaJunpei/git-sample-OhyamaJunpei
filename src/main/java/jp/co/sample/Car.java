274e73b7768382822e04822a51e2d0563b6123b0
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-10-30 11:25:48 +0900

    新規にCarクラスを作成　及びスタートプログラムにコメントを出力するように修正

---
+++ src/main/java/jp/co/sample/Car.java
@@ -0,0 +1,20 @@
+package jp.co.sample;
+
+
+/**
+ * @author rksuser
+ *
+ */
+public class Car {
+	/** スピード */
+	private Integer speed;
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	
+}
