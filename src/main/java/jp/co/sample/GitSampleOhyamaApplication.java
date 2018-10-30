9317484db254229583306779d31c6efb620f2244
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-10-30 10:58:48 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/jp/co/sample/GitSampleOhyamaApplication.java
@@ -0,0 +1,12 @@
+package jp.co.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleOhyamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleOhyamaApplication.class, args);
+	}
+}
